2e86c9f0dd16e2687e8efb204aad493a538442c7
Igor Gomes <igorgomes255@gmail.com>
2023-02-14 11:08:12 -0300

    feat: arrumando server.ts

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,19 @@ import "reflect-metadata";
 import "express-async-errors";
 import express from "express";
 import { handleErrorMiddleware } from "./middlewares/handleError.middleware";
+import { appRoutes } from "./routes/routes";
 
 let cors = require("cors");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+app.get("/", (require, response) => {
+  response.send("Hi folks");
+});
+
+//appRoutes(app);
 
 app.use(handleErrorMiddleware);
 
