623e83d83d183a57faac69a1917e83741ff631c8
Guilherme Silva <2003silvagui@gmail.com>
2023-02-27 01:15:12 -0300

    feat: delete anuncio

--- src/controllers/products/deleteProduct.controller.ts
+++ src/controllers/products/deleteProduct.controller.ts
@@ -1,11 +1,10 @@
 import { Request, Response } from "express";
-import { deleteProductService } from "../../services/products/deleteProduct.service";
+import deleteProductService from "../../services/products/deleteProduct.service";
 
 const deleteProductController = async (req: Request, res: Response) => {
   const { id } = req.params;
-  //const id = req.user.id;
-  // await deleteProductService(id);
-  return res.status(204).json(id);
+  const result = await deleteProductService(id);
+  return res.status(204).send();
 };
 
 export { deleteProductController };
--- src/services/products/deleteProduct.service.ts
+++ src/services/products/deleteProduct.service.ts
@@ -1,7 +1,20 @@
 import AppDataSource from "../../data-source";
 import { Product } from "../../entities/product.entity";
+import { AppError } from "../../errors/appError";
 
-const deleteProductService = async (id: String) => {
+const deleteProductService = async (id: string) => {
+  const productRepository = AppDataSource.getRepository(Product);
+
+  const allProducts = await productRepository.find()
+  const product = allProducts.find((product) => product.id === id)
+
+  if(!product) {
+    throw new AppError('Product not found', 404)
+  }
+    
+  await productRepository.delete(id)
+
+  return 'OK'
   
 };
-export { deleteProductService };
+export default deleteProductService
